b1b40a3e75f5376420c9f2d8ec9699246b4ab674
Srinath-Sarge <sricharu51@gmail.com>
2025-11-21 09:14:59 +0530

    Day-14 Backend Deployment

--- backend/main.py
+++ backend/main.py
@@ -28,3 +28,11 @@ def home():
 @app.get("/user")
 def greet(name: str):
     return {"Name":name}
+
+
+import os 
+import uvicorn
+
+if __name__=="__main__":
+    port =int(os.environ.get("PORT",8000))
+    uvicorn.run("main:app", host="0.0.0.0",port=port)
